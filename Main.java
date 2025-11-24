ad2a4447fe02877960de662de8ad92629bff442f
khanhPham-mm <dinhkhanh09974@gmail.com>
2025-11-24 19:18:20 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("khanh pham");
         System.out.println("replace");
+        System.out.println("new update");
+
     }
 }
